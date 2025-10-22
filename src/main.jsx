c4bdd6ce5cd87bd0f4c510fd705dbda86a85c876
ReyhanYulanda <arimcoding@gmail.com>
2025-10-22 14:30:38 +0700

    add: userregister

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import Layout from './components/layout'
+import UserRegister from './components/user/UserRegister'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element>
-          <Route path='/register'></Route>
+        <Route element={<Layout />}>
+          <Route path='/register' element={<UserRegister />}></Route>
         </Route>
         <Route path='/dashboard'>
 
