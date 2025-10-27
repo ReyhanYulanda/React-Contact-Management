4fe6c9de78b76cba06018355b82826df2a246bb6
ReyhanYulanda <arimcoding@gmail.com>
2025-10-27 16:31:30 +0700

    add: dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import Layout from './components/layout'
 import UserRegister from './components/user/UserRegister'
 import UserLogin from './components/user/UserLogin'
+import DashboardLayout from './components/DashboardLayout'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,8 +14,8 @@ createRoot(document.getElementById('root')).render(
           <Route path='/register' element={<UserRegister />}></Route>
           <Route path='/login' element={<UserLogin />}></Route>
         </Route>
-        <Route path='/dashboard'>
-
+        <Route path='/dashboard' element={<DashboardLayout />}>
+          <Route path='contacts' element={<div>Contacts Page</div>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
